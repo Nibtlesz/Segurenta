9051710396d5961bd294a982139b1e2c4829151a
Emmanuel Garcia Ostria <emmanuel_ostria@hotmail.com>
2020-03-13 09:30:06 -0600

    Correcciones
    se agrega el script del boton inferior
    se modifican las columnas de la pagina para que se pueda ocupar en cualquier pantalla
    se modifica el layout

--- Segurenta/App_Start/BundleConfig.cs
+++ Segurenta/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace Segurenta
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery2").Include(
+                "~/Scripts/jquery-{version}.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -25,6 +28,12 @@ namespace Segurenta
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/arriba").Include(
+                        "~/Scripts/arriba.js"));
+
+            bundles.Add(new StyleBundle("~/Content/fonts").Include(
+                        "~/Fonts/all.css"));
         }
     }
 }
---
+++ Segurenta/Scripts/script.js
@@ -0,0 +1,22 @@
+﻿$$(document).on('ready', function () {
+    $('#enviar_form').click(function (e) {
+        e.preventDefault();
+        var nombre = $('#inputnombre').val().required,
+            correo = $('#inputcorreo').val().required,
+            telefono = $('#inputtelefono').val().required,
+            tipoCliente = $('#inputtip_clie').val().required;
+
+        var url = "controller/contact.php";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#contact-form").serialize(),
+            success: function (data) {
+                document.getElementById('contact-form').reset();
+                $('#enviado_form').html(data);
+
+            }
+        });
+        return false;
+    });
+});
